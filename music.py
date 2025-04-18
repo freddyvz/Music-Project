cdabc9802b41054051d4ee02e498538097e1c857
Freddy <rocetzin4@gmail.com>
2025-04-17 23:15:13 -0700

    Added Window'

--- music.py
+++ music.py
@@ -1,2 +1,8 @@
 import pygame
 import os
+
+pygame.init() #Initialized all pygame modules. Essential to call this before using any Pygame functino
+
+window = pygame.dsiplay.set_mode((500, 300)) # Creates a window with size 500 pixels and 300 pixels tall
+pygame.display.set_caption("Music Player") # Sets title of window
+
