d95d53258bbbe3162ecfb005873687c89855277e
Freddy <rocetzin4@gmail.com>
2025-04-17 23:45:38 -0700

    Fixed Window Bug and Finished GUI

--- music.py
+++ music.py
@@ -2,13 +2,14 @@ import pygame
 import os
 
 pygame.init() #Initialized all pygame modules. Essential to call this before using any Pygame functino
+pygame.mixer.init()
 
-window = pygame.dsiplay.set_mode((500, 300)) # Creates a window with size 500 pixels and 300 pixels tall
+window = pygame.display.set_mode((500, 300)) # Creates a window with size 500 pixels and 300 pixels tall
 pygame.display.set_caption("Music Player") # Sets title of window
 
 # Set Up Colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
+PINK = (255, 182, 193)
+GREEN = (0, 255, 0)
 
 # Sets music_file variable to path of the music file to play
 music_file = "music.mp3" 
@@ -35,5 +36,15 @@ while running:
       if button_x < mouse_x < button_x + button_width and button_y < mouse_y < button_y + button_height: # Checks if mouse click happened inside the bounds of the play button
         play_music()
 
+# Fill the window with white
+  window.fill(PINK)
 
+  pygame.draw.rect(window, GREEN, (button_x, button_y, button_width, button_height))
+  font = pygame.font.Font(None, 36)
+  text = font.render("Play Music", True, PINK)
+  window.blit(text, (button_x + 20, button_y + 10))
+
+  pygame.display.update()
+
+pygame.quit()
 
